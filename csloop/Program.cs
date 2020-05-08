24d6e1eb83c3fa0c6efe28e9ff8584edbcbf73c6
2uijin <49094082+2uijin@users.noreply.github.com>
2020-05-08 12:24:37 +0900

    지렁이가 움직인다아~

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace csloop
@@ -10,21 +11,37 @@ namespace csloop
     {
         static void Main(string[] args)
         {
-            long start = DateTime.Now.Ticks;
-            long cnt = 0;
+            /* long start = DateTime.Now.Ticks;
+             long cnt = 0;
 
-            while (start + (1000000) > DateTime.Now.Ticks)
-            {
-                cnt++;
+             while (start + (1000000) > DateTime.Now.Ticks)
+             {
+                 cnt++;
+             }
+
+             Console.WriteLine(cnt);
+
+
+         string[] array = { "오렌지", "두리아", "배", "딸기", "포도", "용과" };
+         foreach(string item in array){
+                 Console.WriteLine(item);
+         }*/
+
+            int x = 1;
+            while (x < 50) {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if (x % 3 == 0)
+                    Console.WriteLine("__@");
+                else if (x % 3 == 1)
+                    Console.WriteLine("_^@");
+                else
+                    Console.WriteLine("^_@");
+
+                Thread.Sleep(100);
+                x++;
             }
-
-            Console.WriteLine(cnt);
-
-
-        string[] array = { "오렌지", "두리아", "배", "딸기", "포도", "용과" };
-        foreach(string item in array){
-                Console.WriteLine(item);
-        }
 
 
 
