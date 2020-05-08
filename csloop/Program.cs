33d323e596004c3e75bfa486a5083422aaa63f79
2uijin <49094082+2uijin@users.noreply.github.com>
2020-05-08 12:11:46 +0900

    foreach문 연습

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -13,11 +13,24 @@ namespace csloop
             long start = DateTime.Now.Ticks;
             long cnt = 0;
 
-            while (start + (1000000) > DateTime.Now.Ticks) {
+            while (start + (1000000) > DateTime.Now.Ticks)
+            {
                 cnt++;
             }
 
             Console.WriteLine(cnt);
+
+
+        string[] array = { "오렌지", "두리아", "배", "딸기", "포도", "용과" };
+        foreach(string item in array){
+                Console.WriteLine(item);
         }
+
+
+
+        }
+
+
+
     }
 }
